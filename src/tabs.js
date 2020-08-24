c2a00274a2814ba29de051e2dd835f01a74ac831
Dan Kempton <kemptondan@gmail.com>
2020-08-24 15:49:55 -0400

    Adding event listener on tabButtons and creating handleClick callback

--- src/tabs.js
+++ src/tabs.js
@@ -3,3 +3,11 @@ const tabs = document.querySelector('.tabs');
 const tabButtons = tabs.querySelectorAll('.tabButton');
 const tabPanels = Array.from(tabs.querySelectorAll('[role="tabpanel"]'));
 console.log(tabs, tabButtons, tabPanels);
+
+function handleTabButtonClick(event) {
+  console.log(event.target);
+}
+
+tabButtons.forEach((button) =>
+  button.addEventListener('click', handleTabButtonClick)
+);
